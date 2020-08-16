347ac7b361ca259db9dd702ac8dd461ff4779231
Alexis <alexis.fereira@payco.co>
2020-08-15 20:50:28 -0500

    dashboar casi listo

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -5,6 +5,8 @@ import Header from "./header";
 import Flex from "./../UI/Flex";
 import {DegCard} from "./helper";
 import BigCard from "./bigCard";
+import {Icon} from "../landing/bannerInicio/helpers";
+import {colors} from "../UI";
 
 const Container = styled.div`
     position:relative;
@@ -25,6 +27,26 @@ const Container = styled.div`
         margin-bottom:2px;
         margin-right:8px;
     }
+    
+    .datoBlue{
+        background:${colors.cian};
+        border-radius:6px;
+        padding:.3em .75em;
+        text-align:center;
+    }
+    
+    .section,.enlaces{
+        max-width: 200px;
+        margin:auto;
+        .datoBlue{
+            margin:1px auto 5px;
+        }
+    }
+    .enlaces{
+        button{
+            background:transparent;
+        }
+    }
 `;
 
 function Dashboard() {
@@ -34,9 +56,58 @@ function Dashboard() {
             <Datos noDescrtiption />
             <Flex alg={"flex-start"} className={"col-12 col-xl-10 mx-auto cw pt-4 "}>
                 <aside className={"col-12 col-sm-10 col-lg-3 mx-auto pt-5"}>
-                    <DegCard className={"p-3"}>
-
+                    <DegCard className={"p-3 mb-lg-5"}>
+                        <Flex alg={"flex-start"}>
+                            <img src="img/dashboard/hexCian.png" alt="" width={"60px"} height={"auto"}/>
+                            <Flex flex={"1 0 50%"} jc={"flex-end"} className="texto">
+                                <div className={"wc text-right"}>
+                                    <b>ID</b> <span>121523</span>
+                                </div>
+                                <small>
+                                    123
+                                    <img src="img/dashboard/user.png" alt="" width={"15px"} height={"auto"} className={"ml-2 align-middle"}/>
+                                </small>
+                            </Flex>
+                        </Flex>
+                        <div className="datoBlue my-3">
+                            <b>12526254555</b> <span>ETH</span>
+                        </div>
+                        <div className={"datoNormal text-center"}>
+                            <b>$253,622</b> USD
+                        </div>
                     </DegCard>
+
+                    <div className="section py-2 py-lg-3">
+                        <small>BILLIONS <b>X3</b></small>
+                        <div className="datoBlue"><b>{12536252}</b> ETH</div>
+                    </div>
+
+                    <div className="section py-2 py-lg-3 mb-lg-5">
+                        <small>BILLIONS <b>X6</b></small>
+                        <div className="datoBlue"><b>{125326252}</b>ETH</div>
+                        <span>$209,905</span><b>USD</b>
+                    </div>
+
+                    <Flex className="enlaces mb-3">
+                        <Flex flex={"1 0 60%"} className="enlace" jc={"flex-start"}>
+                         <small className={"cw wc"}> Link de afiliado </small>
+                         <small className={"cb"}> asdfasdfasdfasdfdf</small>
+                        </Flex>
+                        <button>
+                            <img src="img/dashboard/copiar.png" width={"25px"} height={"auto"} alt=""/>
+                        </button>
+                    </Flex>
+
+                    <Flex className="enlaces">
+                        <Flex flex={"1 0 60%"} className="enlace" jc={"flex-start"}>
+                           <small className={"cw wc"}> The etherum wallet </small>
+                           <small className={"cb"}>dfgasdfa sdfasdf</small>
+                        </Flex>
+                        <button>
+                            <img src="img/dashboard/copiar.png" width={"25px"} height={"auto"} alt=""/>
+                        </button>
+                    </Flex>
+
                 </aside>
                 <div className="content col-12 col-sm-10 col-lg-9">
                     <BigCard version={"x3"}/>
